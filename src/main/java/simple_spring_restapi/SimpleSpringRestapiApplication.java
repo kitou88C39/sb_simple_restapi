3e8a31549bf81722ab291f3b6662e469ac2c3f9d
kitou88C39 <kanfutrooper@icloud.com>
2023-12-04 09:26:04 +0900

    first commit

---
+++ src/main/java/simple_spring_restapi/SimpleSpringRestapiApplication.java
@@ -0,0 +1,13 @@
+package simple_spring_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringRestapiApplication.class, args);
+	}
+
+}
