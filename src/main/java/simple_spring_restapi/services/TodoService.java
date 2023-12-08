af241eb8b2e2d891df51c693396bf2716d3642f2
kitou88C39 <kanfutrooper@icloud.com>
2023-12-08 20:54:01 +0900

    TodoService.java 編集

--- src/main/java/simple_spring_restapi/services/TodoService.java
+++ src/main/java/simple_spring_restapi/services/TodoService.java
@@ -1,9 +1,17 @@
 package simple_spring_restapi.services;
 
+import java.util.List;
+
 import simple_spring_restapi.models.TodoItem;
 
 public interface TodoService {
     public TodoItem saveTodoItem(TodoItem todoItem);
 
     public List<TodoItem> getTodoItems();
+
+    public TodoItem getTodoItemById(int id);
+
+    public TodoItem updateTodoItems(int id, TodoItem todoItem);
+
+    public void deleteTodoItem(int id);
 }
