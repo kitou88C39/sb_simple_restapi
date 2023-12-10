ba931effbcc74ca330dc4fb83db0e54a85ad00b6
kitou88C39 <kanfutrooper@icloud.com>
2023-12-10 16:26:21 +0900

    NotFoundException.java 編集

--- src/main/java/simple_spring_restapi/controller/errors/NotFoundException.java
+++ src/main/java/simple_spring_restapi/controller/errors/NotFoundException.java
@@ -3,7 +3,10 @@ package simple_spring_restapi.controller.errors;
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException() {
+    }
 
+    public NotFoundException(String message) {
+        super(message);
     }
 
 }
