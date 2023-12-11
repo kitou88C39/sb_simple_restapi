72eeedb5e1545802fedbbe6a109a39f7d196d42b
kitou88C39 <kanfutrooper@icloud.com>
2023-12-11 16:14:31 +0900

    RestResponseEntityExceptionHandler.java 編集

--- src/main/java/simple_spring_restapi/controller/errors/RestResponseEntityExceptionHandler.java
+++ src/main/java/simple_spring_restapi/controller/errors/RestResponseEntityExceptionHandler.java
@@ -3,6 +3,7 @@ package simple_spring_restapi.controller.errors;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,6 +22,9 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @Override
     protected ResponseEntity<Object> handeleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
         return this.handeleExceptionInternal(ex, errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
