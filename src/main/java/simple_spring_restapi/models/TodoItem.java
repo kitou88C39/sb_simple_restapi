a83c26b4445c200fbc2e013f3512b414fd681505
kitou88C39 <kanfutrooper@icloud.com>
2023-12-11 14:48:16 +0900

    models/TodoItem.java 編集

--- src/main/java/simple_spring_restapi/models/TodoItem.java
+++ src/main/java/simple_spring_restapi/models/TodoItem.java
@@ -13,19 +13,4 @@ public class TodoItem {
         this.title = title;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
