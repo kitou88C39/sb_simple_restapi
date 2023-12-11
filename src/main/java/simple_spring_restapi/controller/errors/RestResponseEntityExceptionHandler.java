731af211c4cd26bb49d3d746c74b838f32234623
kitou88C39 <kanfutrooper@icloud.com>
2023-12-11 10:54:52 +0900

    RestResponseEntityExceptionHandler.java 編集

--- src/main/java/simple_spring_restapi/controller/errors/RestResponseEntityExceptionHandler.java
+++ src/main/java/simple_spring_restapi/controller/errors/RestResponseEntityExceptionHandler.java
@@ -1,7 +1,9 @@
 package simple_spring_restapi.controller.errors;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 }
