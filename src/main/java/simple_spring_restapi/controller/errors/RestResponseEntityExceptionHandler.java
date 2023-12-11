090a147026adead635956922164e9d47c1ddb0bf
kitou88C39 <kanfutrooper@icloud.com>
2023-12-11 14:16:40 +0900

    RestResponseEntityExceptionHandler.java 編集

--- src/main/java/simple_spring_restapi/controller/errors/RestResponseEntityExceptionHandler.java
+++ src/main/java/simple_spring_restapi/controller/errors/RestResponseEntityExceptionHandler.java
@@ -1,7 +1,6 @@
 package simple_spring_restapi.controller.errors;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,17 +10,11 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = { NotFoundException.class })
-    protected ResponseEntity<Object> handeleNotFoundException(NotFoundException ex, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(value = { NotFoundException.class, BadRequestException.class })
+    protected ResponseEntity<Object> handeleNotFoundException(HttpException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), ex.getHttpStatus());
         return this.handeleExceptionInternal(ex, errorResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 
     }
 
-    @ExceptionHandler(value = { BadRequestException.class })
-    protected ResponseEntity<Object> handeleBadRequestException(BadRequestException ex, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_FOUND);
-        return this.handeleExceptionInternal(ex, errorResponse, new HttpHeaders(), HttpStatus.BAD_FOUND, request);
-
-    }
 }
