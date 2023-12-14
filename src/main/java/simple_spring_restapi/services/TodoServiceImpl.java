a7b44b58396899aab47ab74dba05f755bee2041a
kitou88C39 <kanfutrooper@icloud.com>
2023-12-14 17:20:42 +0900

    services/TodoServiceImpl.java 編集

--- src/main/java/simple_spring_restapi/services/TodoServiceImpl.java
+++ src/main/java/simple_spring_restapi/services/TodoServiceImpl.java
@@ -1,5 +1,33 @@
 package simple_spring_restapi.services;
 
+import java.util.List;
+
+import com.juniordevmind.simplespringrestapi.models.TodoItem;
+
 public class TodoServiceImpl implements TodoService {
 
+    @Override
+    public Todo getTodo(int id) {
+        return todoDao.getTodo(id);
+    }
+
+    @Override
+    public List<Todo> getTodos() {
+        return todoDao.getTodos();
+    }
+
+    @Override
+    public Todo createTodo(Todo todo) {
+        return todoDao.createTodo(todo);
+    }
+
+    @Override
+    public Todo updateTodo(Todo todo) {
+        return todoDao.updateTodo(todo);
+    }
+
+    @Override
+    public void deleteTodo(int id) {
+        todoDao.deleteTodo(id);
+    }
 }
